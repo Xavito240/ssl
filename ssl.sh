29699f6302f5997b794eab08ece44bd613a3cf53
Xavito <95054430+Xavito240@users.noreply.github.com>
2024-01-21 17:45:23 +0100

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -1,47 +1,50 @@
 #!/bin/bash
 
-# Générer une paire de clés (privée et publique)
-openssl genrsa -out localhost.key 2048
+# Variables
+GLPI_ROOT="/var/www/html/glpi"
+SSL_CERT_PATH="/etc/ssl/certs/glpi.crt"
+SSL_KEY_PATH="/etc/ssl/private/glpi.key"
+APACHE_SITES_AVAILABLE="/etc/apache2/sites-available"
+APACHE_LOG_DIR="/var/log/apache2"
 
-# Créer un certificat auto-signé
-openssl req -new -x509 -key localhost.key -out localhost.crt -days 365 -subj "/CN=localhost"
-
-# Copier les fichiers clés dans le répertoire Apache (assurez-vous que le répertoire existe)
-sudo cp localhost.key /etc/apache2/ssl/private/
-sudo cp localhost.crt /etc/apache2/ssl/certs/
-
-# Modifier la configuration d'Apache pour utiliser le certificat
-cat <<EOF | sudo tee /etc/apache2/sites-available/default-ssl.conf
-<IfModule mod_ssl.c>
-        <VirtualHost _default_:443>
-                ServerAdmin webmaster@localhost
-                DocumentRoot /var/www/html
-
-                SSLEngine on
-                SSLCertificateFile /etc/apache2/ssl/certs/localhost.crt
-                SSLCertificateKeyFile /etc/apache2/ssl/private/localhost.key
-
-                <FilesMatch "\.(cgi|shtml|phtml|php)$">
-                                SSLOptions +StdEnvVars
-                </FilesMatch>
-                <Directory /usr/lib/cgi-bin>
-                                SSLOptions +StdEnvVars
-                </Directory>
-
-                BrowserMatch "MSIE [2-6]" \
-                                nokeepalive ssl-unclean-shutdown \
-                                downgrade-1.0 force-response-1.0
-                BrowserMatch "MSIE [17-9]" ssl-unclean-shutdown
-
-        </VirtualHost>
-</IfModule>
-EOF
-
-# Activer le module SSL
+# Enable Apache SSL module
 sudo a2enmod ssl
 
-# Activer le site SSL
-sudo a2ensite default-ssl.conf
+# Generate SSL certificate
+openssl genrsa -out "$SSL_KEY_PATH" 2048
+openssl req -new -key "$SSL_KEY_PATH" -out /tmp/glpi.csr
+openssl x509 -req -days 365 -in /tmp/glpi.csr -signkey "$SSL_KEY_PATH" -out "$SSL_CERT_PATH"
 
-# Redémarrer Apache pour appliquer les changements
-sudo service apache2 restart
+# Configure Apache virtual host for SSL
+cat <<EOL | sudo tee "$APACHE_SITES_AVAILABLE/glpi-ssl.conf" > /dev/null
+<VirtualHost *:443>
+    ServerAdmin webmaster@localhost
+    DocumentRoot $GLPI_ROOT
+
+    SSLEngine on
+    SSLCertificateFile $SSL_CERT_PATH
+    SSLCertificateKeyFile $SSL_KEY_PATH
+
+    <FilesMatch "\.(cgi|shtml|phtml|php)$">
+        SSLOptions +StdEnvVars
+    </FilesMatch>
+
+    <Directory /usr/lib/cgi-bin>
+        SSLOptions +StdEnvVars
+    </Directory>
+
+    ErrorLog $APACHE_LOG_DIR/error.log
+    CustomLog $APACHE_LOG_DIR/access.log combined
+</VirtualHost>
+EOL
+
+# Enable the SSL virtual host
+sudo a2ensite glpi-ssl
+
+# Restart Apache
+sudo systemctl restart apache2
+
+# Cleanup
+rm /tmp/glpi.csr
+
+echo "SSL configuration for GLPI is complete. Access GLPI using https://your-server"
